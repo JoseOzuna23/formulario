30bb61499804fe7882c14449a58b35f1c9589603
JoseOzuna23 <hose-23@hotmail.com>
2024-02-22 11:13:57 -0300

    pimer comit

---
+++ cliente/src/componente/Principal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Encabezado from './Encabezado'
+import Index from './Index'
+
+const Principal = () => {
+  return (
+    <div className='container-fluid p-0'>
+        <Encabezado/>
+        <Index/>  
+       </div>
+  )
+}
+
+export default Principal
